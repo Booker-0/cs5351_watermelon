f4737976b63641a778bf54b103a87397e78e958f
Alex Warren <42103856+AlexAnthonyWarren@users.noreply.github.com>
2022-11-24 19:39:08 +0800

    Create machine_learning branch
    create machine learning branch

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: [
-    // '@vue/app'
-
-    ['@vue/app', {
-       useBuiltIns: 'entry' // 修改之后
-    }]
-  ]}
--- src/api/taskMember.js
+++
@@ -1,8 +0,0 @@
-import $http from '@/assets/js/http'
-
-export function list(data) {
-    return $http.post('project/task_member/index', data);
-}
-export function inviteMemberBatch(data) {
-    return $http.post('project/task_member/inviteMemberBatch', data);
-}
